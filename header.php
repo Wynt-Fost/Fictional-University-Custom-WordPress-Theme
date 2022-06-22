6c958c96987534e959036a40fdfa4036d4777ab4
Akeem <akeemfoster2@gmail.com>
2022-06-22 16:48:47 -0400

    adding pages and funcationality

--- header.php
+++ header.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?> >
 <head>
+  <meta charset =" <?php bloginfo('charset');  ?>">
+  <!-- need to add this meta tag for your css to be responsive -->
+  <meta name = "viewport" content ="width=device-width, initial-scale=1">
  <?php wp_head(); ?>
+
     
 </head>
-<body>
+<body <?php body_class(); ?>>
 
 <header class="site-header">
       <div class="container">
@@ -15,8 +19,13 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
+            <!-- <?php 
+            wp_nav_menu(array(
+              'theme_location' => 'headerMenuLocation'
+            )); 
+            ?> -->
             <ul>
-              <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+              <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 16 ) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
--- page.php
+++ page.php
@@ -15,20 +15,46 @@ while(have_posts()) {
   </div>
 
   <div class="container container--narrow page-section">
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p>
-        <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a>
-        <span class="metabox__main">Our History</span>
+
+  <!-- this if statement below will evaluate whether you are on a page that has a parent or not. wp_get_post_parent_id will return 0 if it doesnt have a parent and any other number if it does 'get_the_ID()' is getting the id of the page you are on and use that number to get the id of the part page -->
+<?php 
+$theParent = wp_get_post_parent_id(get_the_ID());
+ if( $theParent){ ?>
+ <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a>
+        <span class="metabox__main"><?php the_title(); ?></span>
       </p>
     </div>
+<?php }
+?>
 
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+   
+    <?php 
+    $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+    ));
+    
+    if ($theParent or $testArray ) { ?>
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+      <?php 
+
+      if($theParent){
+        $findCildrenOf = $theParent;
+      }else{
+        $findCildrenOf = get_the_ID();
+      }
+      wp_list_pages(array(
+        'title_li' => NULL,
+        'child_of' => $findCildrenOf,
+            
+      )); 
+
+      ?>
       </ul>
-    </div> -->
+    </div>
+    <?php } ?>
 
     <div class="generic-content">
      
